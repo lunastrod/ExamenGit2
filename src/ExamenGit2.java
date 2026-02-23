f0734aa0fb61bba13d96df1987ffcb6afbb2b721
lunastrod <daniel.parra.segovia@gmail.com>
2026-02-23 10:44:24 +0100

    commit rama1 ff

--- src/ExamenGit2.java
+++ src/ExamenGit2.java
@@ -4,7 +4,7 @@ public class ExamenGit2 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola a tod@s");
-
+		System.out.println("Cambio fast forward");
 	}
 
 }
