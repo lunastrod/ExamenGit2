b80e3c0881b381b840e4a13aade0df3f75ab376c
astrod <44481578+lunastrod@users.noreply.github.com>
2026-02-23 10:49:47 +0100

    cambio remoto

--- src/ExamenGit2.java
+++ src/ExamenGit2.java
@@ -5,6 +5,7 @@ public class ExamenGit2 {
 		System.out.println("Hola mundo");
 		System.out.println("Hola a tod@s");
 		System.out.println("Cambio fast forward");
+		System.out.println("Cambio remoto");
 	}
 
 }
